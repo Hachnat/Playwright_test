20dad3b7d96715b259bde61cda8179eb3c746661
Hachnat <hachnatkhan@gmail.com>
2025-09-22 16:28:04 +0600

    1

--- playwright.config.js
+++ playwright.config.js
@@ -23,11 +23,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-  ['list'], // shows test progress in Jenkins console
-  ['junit', { outputFile: 'playwright-report/results.xml' }], // for Jenkins JUnit plugin
-  ['html', { open: 'never' }] // HTML report for humans
-],
+  reporter: 'list',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
